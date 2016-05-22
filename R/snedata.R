e67c249c86b250b6c016425dfc95827322c0aaaa
James Melville <jlmelville@gmail.com>
2016-05-22 11:10:19 -0700

    Initial Commit.

---
+++ R/snedata.R
@@ -0,0 +1,30 @@
+#' snedata: Dataset functions for Stochastic Neighbor Embedding and related
+#' dimensionality reduction methods.
+#'
+#' @section Simulation functions:
+#' Very simple datasets for characterizing the behavior of various embedding
+#' algorithms.
+#'
+#' @section Faces dataset functions:
+#' If you have the \code{RnavGraphImageData} package
+#' \url{https://cran.r-project.org/web/packages/RnavGraphImageData/index.html}
+#' installed and loaded, then there are functions to convert the Olivetti
+#' and Frey faces datasets into a row-based format, and functions to visualize
+#' the images.
+#'
+#' @note
+#' The faces datasets originate from Saul Roweis' dataset web page:
+#' \url{http://www.cs.nyu.edu/~roweis/data.html}.
+#'
+#' @references
+#' Lee, J. A., Peluffo-Ordo'nez, D. H., & Verleysen, M. (2015).
+#' Multi-scale similarities in stochastic neighbour embedding: Reducing
+#' dimensionality while preserving both local and global structure.
+#' \emph{Neurocomputing}, \emph{169}, 246-261.
+#'
+#' Agrafiotis, D. K., & Xu, H. (2002).
+#' A self-organizing principle for learning nonlinear manifolds.
+#' \emph{Proceedings of the National Academy of Sciences}, \emph{99}(25), 15869-15872.
+#' @docType package
+#' @name snedata
+NULL
---
+++ README.md
@@ -0,0 +1,16 @@
+# snedata
+SNE Dataset Function for R
+
+This package provides functions for generating simple simulation datasets 
+for use in Stochastic Neighbor Embedding and related dimensionality reduction
+methods, similar to those used by Lee and co-workers in their JSE papers.
+
+Additionally, if you have the [RnavGraphImageData](https://cran.r-project.org/web/packages/RnavGraphImageData/index.html)
+package installed, there are also functions to convert the Olivetti and Frey 
+faces datasets into a row-based dataframe and functions to visualize them.
+
+### Installing:
+```R
+# install.packages("devtools")
+devtools::install_github("jlmelville/snedata")
+```
