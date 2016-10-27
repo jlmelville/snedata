3fa7725e977b6706704a7de5669dcf8f541c9dac
James Melville <jlmelville@gmail.com>
2016-10-26 22:58:02 -0700

    Add a color column to the the ball, helix, sphere and swiss roll data. Remove factor columns. Remove the gauss function entirely, replaced by the gaussian_data function.

--- R/simulation.R
+++ R/simulation.R
@@ -6,14 +6,13 @@
 #'
 #' Creates a series of points sampled from a 3D spherical surface.
 #'
-#' Labels go from \code{0} to \code{nlabels - 1}. Points are classified into
-#' labels based on the angle theta in their spherical coordinate presentation.
-#' You can think of it as dividing the sphere surface like the segments of an
-#' orange.
+#' Points are colored based on the angle theta in their spherical coordinate
+#' presentation. You can think of it as dividing the sphere surface like the
+#' segments of an orange.
 #'
 #' @param n The number of points to create.
-#' @param nlabels Number of labels.
-#' @return Data frame.
+#' @return Data frame with \code{x}, \code{y}, \code{z} columns containing the
+#'  coordinates of the points and \code{color} the RGB color.
 #' @references
 #' A dataset like this was used in:
 #' Lee, J. A., Peluffo-Ordo'nez, D. H., & Verleysen, M. (2015).
@@ -21,29 +20,28 @@
 #' dimensionality while preserving both local and global structure.
 #' \emph{Neurocomputing}, \emph{169}, 246-261.
 #' @export
-sphere <- function(n = 1000, nlabels = 5) {
+sphere <- function(n = 1000) {
   # from http://stats.stackexchange.com/questions/7977/how-to-generate-uniformly-distributed-points-on-the-surface-of-the-3-d-unit-sphe
-  z <- runif(n, min = -1, max = 1)
-  theta <- runif(n, min = -pi, max = pi)
+  z <- stats::runif(n, min = -1, max = 1)
+  theta <- stats::runif(n, min = -pi, max = pi)
   x <- sin(theta) * sqrt(1 - z ^ 2)
   y <- cos(theta) * sqrt(1 - z ^ 2)
-  Label <- as.factor(floor(((theta + pi) / (2 * pi)) * nlabels))
-  data.frame(x, y, z, Label)
+
+  data.frame(x, y, z, color = linear_color_map(theta + pi),
+             stringsAsFactors = FALSE)
 }
 
 #' Ball Data Set
 #'
 #' Simulation data.
 #'
-#' Creates a series of points sampled from a 3D spherical volume.
-#'
-#' Labels go from \code{1} to \code{nlabels}. Points are classified into
-#' equally populated labels based on their distance from the origin.
+#' Creates a series of points sampled from a 3D spherical volume. Points are
+#' colored based on the square of their distance from the origin.
 #'
 #' @param n Number of points to create.
 #' @param rad Radius of the ball.
-#' @param nlabels Number of labels.
-#' @return Data frame.
+#' @return Data frame with \code{x}, \code{y}, \code{z} columns containing the
+#'  coordinates of the points and \code{color} the RGB color.
 #' @references
 #' A dataset like this was used in:
 #' Lee, J. A., Peluffo-Ordo'nez, D. H., & Verleysen, M. (2015).
@@ -51,13 +49,15 @@ sphere <- function(n = 1000, nlabels = 5) {
 #' dimensionality while preserving both local and global structure.
 #' \emph{Neurocomputing}, \emph{169}, 246-261.
 #' @export
-ball <- function(n = 1000, rad = 1, nlabels = 5) {
+ball <- function(n = 1000, rad = 1) {
   # from http://math.stackexchange.com/questions/87230/picking-random-points-in-the-volume-of-sphere-with-uniform-probability
-  u <- runif(n)
-  xyz <- matrix(rnorm(3 * n), nrow = n,
+  u <- stats::runif(n)
+  xyz <- matrix(stats::rnorm(3 * n), nrow = n,
                 dimnames = list(NULL, c("x", "y", "z")))
+
   df <- data.frame((xyz * rad * u ^ (1 / 3)) / sqrt(rowSums(xyz ^ 2)))
-  df$Label <- equal_factors(sqrt(rowSums(df ^ 2)), nlabels)
+  dist2 <- rowSums(df ^ 2)
+  df$color <- linear_color_map(dist2)
   df
 }
 
@@ -71,23 +71,28 @@ ball <- function(n = 1000, rad = 1, nlabels = 5) {
 #' Unlike \code{\link{ball}} and \code{\link{sphere}}, this data set is not
 #' randomly sampled.
 #'
-#' Labels go from \code{1} to \code{nlabels}. Points are classified into
-#' equally-populated labels based on their z-coordinate.
+#' Points are colored based on their distances from the origin.
 #'
 #' @param n Number of points to create.
 #' @param rmajor Major radius.
 #' @param rminor Minor radius.
 #' @param nwinds Number of winds the helix makes.
-#' @param nlabels Number of labels.
-#' @return Data frame.
+#' @return Data frame with \code{x}, \code{y}, \code{z} columns containing the
+#'  coordinates of the points and \code{color} the RGB color.
 #' @references
 #' A dataset like this was used in:
 #' Lee, J. A., Peluffo-Ordo'nez, D. H., & Verleysen, M. (2015).
 #' Multi-scale similarities in stochastic neighbour embedding: Reducing
 #' dimensionality while preserving both local and global structure.
 #' \emph{Neurocomputing}, \emph{169}, 246-261.
+#' @examples
+#' \dontrun{
+#'  helix1000 <- helix(n = 1000)
+#'  # Coloring should be obvious with a 2D plot of the cross section:
+#'  plot(helix1000$x, helix1000$y, col = helix1000$color, pch = 20)
+#' }
 #' @export
-helix <- function(n = 1000, rmajor = 2, rminor = 1, nwinds = 8, nlabels = 5) {
+helix <- function(n = 1000, rmajor = 2, rminor = 1, nwinds = 8) {
   # http://math.stackexchange.com/questions/324527/do-these-equations-create-a-helix-wrapped-into-a-torus
   u <- seq(-pi, pi, length.out = n)
   w <- rmajor + (rminor * cos(nwinds * u))
@@ -95,34 +100,12 @@ helix <- function(n = 1000, rmajor = 2, rminor = 1, nwinds = 8, nlabels = 5) {
   y <- w * sin(u)
   z <- rminor * sin(nwinds * u)
 
-  Label <- equal_factors(z, nlabels)
-  data.frame(x, y, z, Label)
-}
+  df <- data.frame(x, y, z)
 
-#' d-Dimensional Gaussian Data Set
-#'
-#' Simulation data.
-#'
-#' Creates a series of points randomly sampled from an d-Dimensional Gaussian
-#' with mean zero and variance 1.
-#'
-#' Labels go from \code{0} to \code{nlabels - 1}. Points are classified into
-#' labels based on their distance from the origin, where that distance is
-#' divided into \code{nlabels} equal lengths. That results in unequal volumes
-#' and hence unequal populations of the labels.
-#'
-#' @param n Number of points to create.
-#' @param d Dimension of the Gaussian.
-#' @param nlabels Number of labels.
-#' @return Data frame.
-#' @export
-gauss <- function(n = 1000, d = 3, nlabels = 5) {
-  m <- matrix(rnorm(n * d), ncol = d)
-  dist <- sqrt(rowSums(m * m))
+  dist <- sqrt(rowSums(df ^ 2))
+  df$color <- linear_color_map(dist)
 
-  Label <- equal_factors(dist, nlabels)
-
-  data.frame(m, Label)
+  df
 }
 
 #' Swiss Roll Data Set
@@ -142,24 +125,29 @@ gauss <- function(n = 1000, d = 3, nlabels = 5) {
 #' where \eqn{\phi}{phi} and z are random numbers in the intervals [5, 13]
 #' and [0, 10], respectively.
 #'
-#' Labels go from \code{0} to \code{nlabels - 1}. Points are classified into
-#' labels based on their z-coordinate.
+#' Points are colored based on the value of phi. If you unrolled the manifold
+#' into a flat sheet, you would see the color change linearly in the direction
+#' you unrolled it. Or just plot the x-y cross section (see the examples).
 #'
 #' @param n Number of points to create.
-#' @param nlabels Number of labels.
-#' @return Data frame.
+#' @return Data frame with \code{x}, \code{y}, \code{z} columns containing the
+#'  coordinates of the points and \code{color} the RGB color.
 #' @references
 #' Agrafiotis, D. K., & Xu, H. (2002).
 #' A self-organizing principle for learning nonlinear manifolds.
 #' \emph{Proceedings of the National Academy of Sciences}, \emph{99}(25), 15869-15872.
+#' @examples
+#' \dontrun{
+#'  swiss1000 <- swiss_roll(n = 1000)
+#'  # Coloring should be obvious with a 2D plot of the cross section:
+#'  plot(swiss1000$x, swiss1000$y, col = swiss1000$color, pch = 20)
+#' }
 #' @export
-swiss_roll <- function(n = 1000, nlabels = 6) {
-  phi <- runif(n, min = 5, max = 30)
+swiss_roll <- function(n = 1000) {
+  phi <- stats::runif(n, min = 5, max = 30)
   x <- phi * cos(phi)
   y <- phi * sin(phi)
-  z <- runif(n, max = 10)
+  z <- stats::runif(n, max = 10)
 
-  Label <- as.factor(ceiling(((phi - 5) / 25) * nlabels - 1))
-
-  data.frame(x, y, z, Label)
+  data.frame(x, y, z, color = linear_color_map(phi), stringsAsFactors = FALSE)
 }
